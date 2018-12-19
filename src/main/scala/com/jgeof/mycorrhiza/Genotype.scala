9c636dbdac9ef3cc90517bf6a679cc0635161e21
jeremy <jeremy.geo@gmail.com>
2018-12-19 15:58:08 +0100

    Improve bitset building

---
+++ src/main/scala/com/jgeof/mycorrhiza/Genotype.scala
@@ -0,0 +1,56 @@
+package com.jgeof.mycorrhiza
+
+import scala.collection.{mutable => m}
+import com.jgeof.mycorrhiza.Exceptions._
+
+class Genotype(rawGenotype: String) extends Iterable[Int] {
+    import Genotype._
+
+    def numLoci: Int = rawGenotype.length
+    val (genotype , invalid) = stringToBitset(rawGenotype)
+
+    def valid(a:Genotype, b:Genotype):Int = numLoci-(a.invalid|b.invalid).size
+    def same(a:Genotype, b:Genotype):Int = (a.genotype&b.genotype).size
+
+
+    override def iterator: Iterator[Int] = genotype.toIterator
+    def ==(that: Genotype): Float = same(this, that)
+    def =?(that: Genotype): Float = valid(this, that)
+
+
+    def stringToBitset(chars: String): (m.BitSet, m.BitSet) = {
+        val set = m.BitSet.empty
+        val iv = m.BitSet.empty
+
+        for(i <- chars.indices){
+            val c = chars.charAt(i)
+            c match {
+                case 'A' => set + ((AlphaSize * i) + 0);
+                case 'T' => set + ((AlphaSize * i) + 1);
+                case 'G' => set + ((AlphaSize * i) + 2);
+                case 'C' => set + ((AlphaSize * i) + 3);
+                case 'N' => iv + i
+                case c: Char => throw InvalidCharacter(c.toString)
+            }
+        }
+        (set, iv)
+    }
+
+}
+
+object Genotype {
+
+    val AlphaSize = 4
+
+    def naiveDistance(a: String, b: String): Float = {
+        val same = a.zip(b).map({
+            case ('N',_) => 0
+            case (_,'N') => 0
+            case (y,x) if x==y => 1
+            case _ => 0
+        })
+        val valid = a.zip(b).count(x => x._2!='N' && x._1!='N')
+        (valid.toFloat-same.sum)/valid.toFloat
+    }
+}
+
