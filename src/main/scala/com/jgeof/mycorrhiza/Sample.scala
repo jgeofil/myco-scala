1d23c39709039578733261787305f144c622807f
jeremy <jeremy.geo@gmail.com>
2018-12-19 17:49:20 +0100

    printing

--- src/main/scala/com/jgeof/mycorrhiza/Sample.scala
+++ src/main/scala/com/jgeof/mycorrhiza/Sample.scala
@@ -16,12 +16,15 @@ object Sample {
         val sampleBuffer = ArrayBuffer.empty[Sample]
         var t0 = System.nanoTime()
         var sampleCount = 0
+        var thresh = 20
         for(str <- lines) {
             val arr = str.split("\t")
             sampleBuffer.append(new Sample(arr.head, arr(1), arr.last))
             sampleCount += 1
-            if(sampleCount % 20 == 0){
-                println(s"Reading ${20f/((System.nanoTime()-t0)*1e-9)} samples per second, with $sampleCount done...")
+            if(sampleCount % thresh == 0){
+                val perSecond = thresh/((System.nanoTime()-t0)*1e-9)
+                thresh = perSecond.toInt*10
+                println(s"Reading $perSecond samples per second, with $sampleCount done...")
                 t0 = System.nanoTime()
             }
         }
