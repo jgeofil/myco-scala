b8f71942d3dbc08465dab2ea096bc0faad2e823f
jeremy <jeremy.geo@gmail.com>
2018-12-20 01:41:45 +0100

    Timer

---
+++ src/main/scala/com/jgeof/mycorrhiza/Timer.scala
@@ -0,0 +1,12 @@
+package com.jgeof.mycorrhiza
+
+object Timer {
+
+    def time[R](name: String)(block: => R): R = {
+        val t0 = System.nanoTime()
+        val result = block
+        val t1 = System.nanoTime()
+        println(s"Elapsed time for $name: " + (t1 - t0)*1e-9 + "s")
+        result
+    }
+}
